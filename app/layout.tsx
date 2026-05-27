aa54d07267c1a5adaec20c3ededce841eeaecee4
Praveen Saravanan <praveen747757@gmail.com>
2026-05-27 15:40:08 +0530

    fix: change Google Tag Manager script loading strategy to beforeInteractive and move content to dangerouslySetInnerHTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,13 +33,18 @@ export default function RootLayout({
   return (
     <html lang='en' className={dmSans.variable} suppressHydrationWarning>
       <head>
-        <Script id='gtm-script' strategy='afterInteractive'>
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Script
+          id='gtm-script'
+          strategy='beforeInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PWF5LHKF');`}
-        </Script>
+})(window,document,'script','dataLayer','GTM-PWF5LHKF');`,
+          }}
+        />
       </head>
       <body className={dmSans.className}>
         <noscript>
